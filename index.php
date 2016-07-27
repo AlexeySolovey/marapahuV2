71be61e6c8bcc5944ec10e7c822b9a22712d7245
Gorchak_UA <gorchakua@gmail.com>
2016-07-27 15:57:56 +0300

    UTF-8 for javascript

--- index.php
+++ index.php
@@ -266,11 +266,7 @@
 	<?php include_once "footer.php";?>
 
 
-    <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/bxslider.min.js"></script>
-    <script type="text/javascript" src="js/common.js"></script>
-    <script src="js/jquery.scrollUp.js"></script>
+
 
 </body>
 </html>
